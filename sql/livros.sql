b93246ae5edce6ee2b1840d8da3c0446be078484
Anna Levra <125038135+annalevra@users.noreply.github.com>
2025-06-10 20:38:57 -0300

    Atualizacao de tabelas

--- sql/livros.sql
+++ sql/livros.sql
@@ -1,22 +1,11 @@
--- Tabela de Lançamentos Financeiros
-/*create table lancamentos(
-  id uuid primary key default gen_random_uuid(),
-  user_id uuid references auth.users on delete cascade,
-  tipo text check (tipo in ('receita','despesa')),
-  categoria text,
-  valor numeric not null check (valor >= 0),
-  descricao text,
-  data_ocorrencia date not null,
-  data_vencimento date,
-  created_at timestamp with time zone default timezone('utc'::text, now())
-);*/
 
+-- Tabela de Lançamentos de Livros
 create table livros(
   id uuid primary key default gen_random_uuid(),
   user_id uuid references auth.users on delete cascade,
-  tipo text check (tipo in ('romance','ficcao','estudo','outros')),
-  categoria text,
-  descricao text,
+  titulo text,
+  autor text,
+  categoria text check (categoria in ('romance','ficcao','estudo','outros')),
   data_locacao date not null,
   data_devolucao date,
   created_at timestamp with time zone default timezone('utc'::text, now())
