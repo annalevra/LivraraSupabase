3f1f00f1ab8765dbb224f7a5f594823946ad49ce
Anna Levra <125038135+annalevra@users.noreply.github.com>
2025-06-10 19:33:39 -0300

    Atualizacao

--- sql/lancamentos.sql
+++ sql/lancamentos.sql
@@ -1,5 +1,5 @@
 -- Tabela de Lançamentos Financeiros
-create table lancamentos(
+/*create table lancamentos(
   id uuid primary key default gen_random_uuid(),
   user_id uuid references auth.users on delete cascade,
   tipo text check (tipo in ('receita','despesa')),
@@ -9,11 +9,24 @@ create table lancamentos(
   data_ocorrencia date not null,
   data_vencimento date,
   created_at timestamp with time zone default timezone('utc'::text, now())
+);*/
+
+create table livros(
+  id uuid primary key default gen_random_uuid(),
+  user_id uuid references auth.users on delete cascade,
+  tipo text check (tipo in ('romance','ficcao','estudo','outros')),
+  categoria text,
+  descricao text,
+  data_locacao date not null,
+  data_devolucao date,
+  created_at timestamp with time zone default timezone('utc'::text, now())
 );
+
 --Habilitar segurança do supabase
-alter table lancamentos enable row level security;
+alter table livros enable row level security;
 --Política para cada usuário somente ver os seus próprios lançamentos
-create policy "Permitir acesso ao próprio usuário no lançamento"
-on lancamentos for all
+create policy "Permitir acesso ao próprio usuário no cadastro de livros"
+  on livros for select
+on livros for all
 using (auth.uid() = user_id)
 with check (auth.uid() = user_id);
